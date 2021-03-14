17d7023623ae18df0bc5932a66e707c548318d40
Grant <h.grant.horner@gmail.com>
2021-03-14 14:51:17 -0400

    Can create todos in the server

--- lib/server.ex
+++ lib/server.ex
@@ -1,10 +1,13 @@
 defmodule Todo.Server do
 	use GenServer
+	require Record
+	Record.defrecord(:todo, title: "", description: "")
+	@type todo :: record(:todo, title: String.t(), description: String.t())
 
 	# Client
 
-	def start_link do
-		GenServer.start_link(__MODULE__, [])
+	def start_link(state) when is_list(state) do
+		GenServer.start_link(__MODULE__, state)
 	end
 
 	def call(pid) do
@@ -15,6 +18,11 @@ defmodule Todo.Server do
 		GenServer.cast(pid, :cast)
 	end
 
+	def create(pid, title, description) do
+		todo = todo(title: title, description: description)
+		GenServer.call(pid, {:create, todo})
+	end
+
 	# Server
 
 	@impl true
@@ -23,12 +31,18 @@ defmodule Todo.Server do
 	end
 
 	@impl true
-	def handle_call(msg, _from, state) do
+	def handle_call({:create, todo}, _from, state) do
+		new_state = [todo | state]
+		{:reply, new_state, new_state}
+	end
+
+	@impl true
+	def handle_call(_msg, _from, state) do
 		{:reply, :my_reply, state}
 	end
 
 	@impl true
-	def handle_cast(msg, state) do
-		{:no_reply, state}
+	def handle_cast(_msg, state) do
+		{:noreply, state}
 	end
 end
--- test/todoserver_test.exs
+++ test/todoserver_test.exs
@@ -4,12 +4,33 @@ defmodule TodoServerTest do
 	alias Todo.Server
 
 	setup do
-		{:ok, pid} = Server.start_link()
+		pid = start_supervised!(Server)
 		[pid: pid]
 	end
 
-	test "exists", %{pid: p} do
+	test "can call", %{pid: p} do
 		res = Server.call(p)
 		assert res == :my_reply
+	end
+
+	test "can cast", %{pid: p} do
+		Server.cast(p)
+		assert :ok == :ok
+	end
+
+	test "can create a todo", %{pid: p} do
+		todos = Server.create(p, "my title", "my description")
+		[todo] = todos
+		assert length(todos) == 1
+		assert Server.todo(todo, :title) == "my title"
+		assert Server.todo(todo, :description) == "my description"
+	end
+
+	test "can create another todo without sharing state", %{pid: p} do
+		todos = Server.create(p, "my title", "my description")
+		[todo] = todos
+		assert length(todos) == 1
+		assert Server.todo(todo, :title) == "my title"
+		assert Server.todo(todo, :description) == "my description"
 	end
 end
